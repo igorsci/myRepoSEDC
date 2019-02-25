114cf4accbe7c1c38f2b29958accc37382618afb
Igor Mitevski <37457893+igorsci@users.noreply.github.com>
2019-02-26 00:41:28 +0100

    Add files via upload

---
+++ basic C#/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Thread.Sleep(2000);
+            Console.WriteLine("prepare");
+            Console.Clear();
+            Thread.Sleep(3000);
+            Console.WriteLine("GO");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Thread.Sleep(5000);
+
+            //Console.ReadKey();
+        }
+    }
+}
