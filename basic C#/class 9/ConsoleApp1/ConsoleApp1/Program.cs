fce369b986182df8697c5af572af3c10234d62ac
Igor Mitevski <37457893+igorsci@users.noreply.github.com>
2019-03-20 21:08:52 +0100

    Add files via upload

---
+++ basic C#/class 9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ConsoleApp1.classes
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Cinema cinema = new Cinema("Cineplexx", 10);
+            
+
+        }
+    }
+}
---
+++ basic C#/class 9/ConsoleApp1/ConsoleApp1/classes/Movie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.classes
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Rating { get; set; }
+        public int TicketPrice { get; set; }
+        public Movie(string title, Genre genre,int rating)
+        {
+            Title = title;
+            Genre = genre;
+            if (rating > 5 || rating < 1)
+            {
+                throw new Exception("the rating  should be between 1-5");
+            } else  {
+                Rating = rating;
+            }
+            TicketPrice = rating * 5;
+        }
+
+    }
+
+    public enum Genre
+    {
+        Comedy,
+            Horor,
+            Action,
+            Drama,
+            SciFi
+    }
+
+    public class Cinema
+    {
+        public string Name;
+        public int Halls;
+        public List<Movie> ListOfMovies;
+        
+        public void MoveiPlaying(Movie movie)
+        {
+            Console.WriteLine($"the move{movie.Title} is playing");
+        }
+
+        public Cinema(string name, int numOfHalls)
+        {
+            Name = name;
+            Halls = numOfHalls;
+            
+        }
+    }
+
+}
+
